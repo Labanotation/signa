e862ca5a7323202dbfa01c8af889535fe21c8cc7
LePhasme <sebastien.courvoisier@gmail.com>
2020-05-29 00:20:13 +0200

    Testing toasts and adding missing fonts for Firefox (WIP)...

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,22 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
+  $('.toast').toast({
+    autohide: false
+  })
+  checkAdBlocker()
 })
+
+function checkAdBlocker () {
+  var test = document.createElement('div')
+  test.innerHTML = '&nbsp;'
+  test.className = 'adsbox'
+  document.body.appendChild(test)
+  window.setTimeout(() => {
+    if (test.offsetHeight === 0) {
+      document.body.classList.add('adblocked')
+      $('#toast-adblocker').toast('show')
+    }
+    test.remove()
+  }, 100)
+}
